32389391f70f3dc92f48b59c33a8d2cf4a9071cb
BeatrizLimaS2 <134568548+BeatrizLimaS2@users.noreply.github.com>
2023-10-25 23:11:35 -0300

    Update marezinha.cpp

--- marezinha.cpp
+++ marezinha.cpp
@@ -9,13 +9,53 @@
 #define PIN_IN_CONTROL_2 0
 
 #define PIN_OUT_CONTROL_ERROR 0
-
 #define PIN_OUT_BUZZER 0 
-
 #define PIN_OUT_HEADLIGHT 0
-
-#define PIN__CURRENT_SENSOR 0
+#define PIN_CURRENT_SENSOR 0
 
 #define PIN_SHUTDOWN 0
 
+
+void setup ()
+{
+  Serial.begin(9600);
+  pinMode(PIN_OUT_PWM_MOTOR_RIGHT , OUTPUT);
+  pinMode(PIN_OUT_PWM_MOTOR_LEFT , OUTPUT);
+  pinMode(PIN_OUT_CHARGE_SENSOR , OUTPUT);
+  
+  pinMode(PIN_IN_CONTROL_1 , INPUT);
+  pinMode(PIN_IN_CONTROL_2 , INPUT);
+
+  pinMode(PIN_OUT_CONTROL_ERROR , OUTPUT);
+  pinMode(PIN_OUT_BUZZER , OUTPUT);
+  pinMode(PIN_OUT_HEADLIGHT, OUTPUT);
+
+  pinMode(PIN_CURRENT_SENSOR, INPUT);
+
+  pinMode(PIN_SHUTDOWN, OUTPUT);
+}
+
+bool controlStatus = false;
+
+
+void moviment ()
+{
+  
+}
+
+void check_control ()
+{
+  while (controlStatus == false)
+  {
+
+    if ()
+      controlStatus = true;
+  }
+
+}
+
+void loop () 
+{
+  
+}
 
