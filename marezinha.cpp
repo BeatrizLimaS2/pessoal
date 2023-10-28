ebb145a730a43ce72c7c6ced9fff006e38ee08e1
BeatrizLimaS2 <134568548+BeatrizLimaS2@users.noreply.github.com>
2023-10-28 14:26:08 -0300

    Update marezinha.cpp

--- marezinha.cpp
+++ marezinha.cpp
@@ -34,90 +34,96 @@ void setup ()
 
   pinMode(PIN_SHUTDOWN, OUTPUT);
 
- 	attachInterrupt(digitalPinToInterrupt(PIN_IN_CONTROL_FORWARD_BACKWARD), risingForBack, RISING);
- 	attachInterrupt(digitalPinToInterrupt(PIN_IN_CONTROL_LEFT_RIGHT), risingLeftRight, RISING);
+	 attachInterrupt(digitalPinToInterrupt(PIN_IN_CONTROL_FORWARD_BACKWARD), risingForBack, RISING);
+	 attachInterrupt(digitalPinToInterrupt(PIN_IN_CONTROL_LEFT_RIGHT), risingLeftRight, RISING);
+
+ digitalWrite(PIN_CURRENT_SENSOR, HIGH);
 }
 
 int contador = -1;
 int direcao;
 long int risingForBackTime, risingLeftRightTime, baseTime;
-int pulseForBack, pulseLeftRight;
+int pulseForBack, pulseLeftRight, pulsoForBackTime, pulsoLeftRightTime;
 
 bool controlStatus = false;
 
 
 void risingForBack()
 {
-	contador++;
- 	risingForBackTime = micros();
-  	attachInterrupt(digitalPinToInterrupt(PIN_IN_CONTROL_FORWARD_BACKWARD), fallingForBack, FALLING); 
+  contador++;
+  risingForBackTime = micros();
+  attachInterrupt(digitalPinToInterrupt(PIN_IN_CONTROL_FORWARD_BACKWARD), fallingForBack, FALLING);
 }
 
 void fallingForBack()
 {
- 	pulsoForBack = micros() - risingForBackTime;
-  	attachInterrupt(digitalPinToInterrupt(PIN_IN_CONTROL_FORWARD_BACKWARD), risingForBack, RISING); 
+	pulseForBack = micros() - risingForBackTime;
+  attachInterrupt(digitalPinToInterrupt(PIN_IN_CONTROL_FORWARD_BACKWARD), risingForBack, RISING);
 }
 
 void risingLeftRight()
 {
-	contador++;
- 	risingLeftRightTime = micros();
-	attachInterrupt(digitalPinToInterrupt(PIN_IN_CONTROL_LEFT_RIGHT), fallingLeftRight, FALLING); 
+  contador++;
+  risingLeftRightTime = micros();
+  attachInterrupt(digitalPinToInterrupt(PIN_IN_CONTROL_LEFT_RIGHT), fallingLeftRight, FALLING);
 }
 
 void fallingLeftRight()
 {
- 	pulsoLeftRight= micros() - risingLeftRightTime;
-  	attachInterrupt(digitalPinToInterrupt(PIN_IN_CONTROL_LEFT_RIGHT), risingLeftRight, RISING); 
+  pulseLeftRight= micros() - risingLeftRightTime;
+  attachInterrupt(digitalPinToInterrupt(PIN_IN_CONTROL_LEFT_RIGHT), risingLeftRight, RISING);
 }
 
 void moviment ()
 {
-  	if (contador == 0 && pulsoForBackTime > 0)
-    		pulsoBase = pulsoForBackTime;
-  	else if (contador == 0 && pulsoLeftRightTime > 0)
-    		pulsoBase = pulsoLeftRightTime;
+  int pulsoBase;
+  long y;
+  long x;
+  long zero;
 
-	zero = map(pulsoBase, 1000, 2000, -100, 100);
-	y= map(pulsoCH3, 1000, 2000, -100, 100);
-  	x= map(pulsoCH1, 1000, 2000, -100, 100);
-	
-	if (y < zero) // andando pra tras
+  if (contador == 0 && pulsoForBackTime > 0)
+    pulsoBase = pulsoForBackTime;
+  else if (contador == 0 && pulseLeftRight > 0)
+    pulsoBase = pulsoLeftRightTime;
 
-	else // andando pra frente
+    zero = map(pulsoBase, 1000, 2000, -100, 100);
+    y= map(pulseForBack, 1000, 2000, -100, 100);
+    x= map(pulseLeftRight, 1000, 2000, -100, 100);
+    
+    if (y < zero) // andando pra tras
 
-	if (x < zero) //indo para a esquerda
-		
-	else //indo para a direita
-		
+    else // andando pra frente
+
+    if (x < zero) //indo para a esquerda
+   	 
+    else //indo para a direita
+   	 
 }
 
 void current_read()
 {
-	digitalWrite(PIN_CURRENT_SENSOR, HIGH);
-	while true
-	{
-		valor = analogRead((pin_sensor)
-             	if (valor >3.0){
-              		digitalWrite(PIN_CURRENT_SENSOR, LOW);}
-        }
-} 
+    while true
+    {
+   	 valor = analogRead((pin_sensor)
+        		 if (valor >3.0){
+         			 digitalWrite(PIN_CURRENT_SENSOR, LOW);}
+    	}
+}
 
 void check_control ()
 {
   while (controlStatus == false)
   {
-	if (pulseForBack > ??? || pulseLeftRight > ???)
-  		controlStatus = true;
+    if (pulseForBack > ??? || pulseLeftRight > ???)
+     	 controlStatus = true;
   }
 }
 
 void loop ()
 {
-	check_control();
-	current_read();
-	moviment();
+    check_control();
+    current_read();
+    moviment();
 
 }
 
